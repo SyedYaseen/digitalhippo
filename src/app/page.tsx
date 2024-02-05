35870feedcc63a9635313d92f7c5c6fa317f738b
TheRealYaseen <s.syedyaseen.s@gmail.com>
2024-02-05 12:43:21 +0000

    edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,25 @@
 import Image from "next/image";
 import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+import Link from "next/link";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Ghost } from "lucide-react";
 
 
 export default function Home() {
   return (
     <>
     <MaxWidthWrapper>
-    <div className="flex flex-col border-4 border-red-500 py-20 text-center mx-auto max-w-3xl">
+    <div className="flex flex-col  py-20 text-center mx-auto max-w-3xl">
       <h1 className="text-4xl sm:text-6xl bold">your marketplace for {' '}
       <span className="text-blue-400">
       high quality assets
       </span></h1>
-      <p>Testing push</p>
+      <div className="flex flex-col mt-6 gap-4">
+      <Link href='/products' className={buttonVariants()}>
+        Browse trending</Link>
+        <Button variant='ghost'>try stuff</Button>
+        </div>
+
     </div>
     </MaxWidthWrapper>
     </>
--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -1,9 +1,10 @@
 import {cn} from '@/lib/utils'
+import { ReactNode } from 'react'
 const MaxWidthWrapper = ({className, children}: 
     {className?: string
        children: ReactNode}) => {
   return (
-    <div className={cn("mx-auto bg-slate-500 max-w-scren-xl px-2.5 md:px-20", className)} >{children}</div>
+    <div className={cn("mx-auto max-w-scren-xl px-2.5 md:px-20", className)} >{children}</div>
   )
 }
 
