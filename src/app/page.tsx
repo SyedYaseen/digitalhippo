465fbc3a30062b25ac1752bcaf798b12b1eeedc6
TheRealYaseen <s.syedyaseen.s@gmail.com>
2024-02-05 11:56:04 +0000

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,11 @@ export default function Home() {
   return (
     <>
     <MaxWidthWrapper>
-    <div className="flex flex-col py-20 text-center mx-auto">
-      <h1>SOmething</h1>
+    <div className="flex flex-col border-4 border-red-500 py-20 text-center mx-auto max-w-3xl">
+      <h1 className="text-4xl sm:text-6xl bold">your marketplace for {' '}
+      <span className="text-blue-400">
+      high quality assets
+      </span></h1>
     </div>
     </MaxWidthWrapper>
     </>
